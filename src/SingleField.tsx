94c1d28528f5a1b6cfdeadfb45da12d1b1af9759
Daniel Sitali <reconvergence@hotmail.com>
2020-12-13 21:19:07 +0000

    fixed types for DoubleField and SingleField

--- src/SingleField.tsx
+++ src/SingleField.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 
 interface Props {
-  options: [string]
-  setOptions: (x: any) => {}
+  options: string[]
+  setOptions: (x: any) => {} | void
   label: string
 }
 
